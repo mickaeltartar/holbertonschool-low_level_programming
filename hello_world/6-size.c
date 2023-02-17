847e85f82be8c6be66e50dca9d7ff44c38d42dbb
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-16 23:48:50 -0800

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - fonction principal void = zero argument
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
+printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
+printf("Size of Long Double Data Types in C = %2d bytes \n", sizeof(double));
+printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
+return (0);
+}
