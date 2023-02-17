4091fd571b66880e865efdfcdced438ba9ac6d80
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-17 01:27:12 -0800

    6-size.c5

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,15 +9,10 @@
 
 int main(void)
 {
-char charType;
-int integerType;
-long longType;
-double doubleType;
-float floatType;
-printf("Size of a char: %2d byte(s)\n", sizeof(char));
-printf("Size of an int: %2d byte(s)\n", sizeof(int));
-printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
-printf("Size of a long long: %2d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %2d byte(s)\n", sizeof(float));
+printf("Size of a char: %2dbyte(s)\n", sizeof(char));
+printf("Size of an int: %2dbyte(s)\n", sizeof(int));
+printf("Size of a long int: %2dbyte(s)\n", sizeof(long int));
+printf("Size of a long long: %2dbyte(s)\n", sizeof(long long int));
+printf("Size of a float: %2dbyte(s)\n", sizeof(float));
 return (0);
 }
