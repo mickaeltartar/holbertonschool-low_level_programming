da9abfb365d6d70b767d511688d476a0ce08bcaf
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-17 00:03:07 -0800

    6-size.c1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,7 +13,7 @@ float floatType;
 double doubleType;
 char charType;
 printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
-printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
+printf("Size of Int Data Types in C = %2d bytes \n", sizeof(int));
 printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
 printf("Size of Long Double Data Types in C = %2d bytes \n", sizeof(double));
 printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
