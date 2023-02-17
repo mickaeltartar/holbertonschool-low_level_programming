b9a26ae6d1ccd8d9586bd3181d05bea7fb1948ee
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-17 01:22:55 -0800

    6-size.c4

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  *main - fonction principal void = zero argument
+ *sizeof - size of file in octet
  *
  *Return: always 0
  */
@@ -13,10 +14,10 @@ int integerType;
 long longType;
 double doubleType;
 float floatType;
-printf("Size of a char: %2d byte(s) \n", sizeof(char));
-printf("Size of an int: %2d byte(s) \n", sizeof(int));
-printf("Size of a long int: %2d byte(s) \n", sizeof(long int));
-printf("Size of a long long: %2d byte(s) \n", sizeof(long long int));
-printf("Size of a float: %2d byte(s) \n", sizeof(float));
+printf("Size of a char: %2d byte(s)\n", sizeof(char));
+printf("Size of an int: %2d byte(s)\n", sizeof(int));
+printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+printf("Size of a long long: %2d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %2d byte(s)\n", sizeof(float));
 return (0);
 }
