3a7c38e55cbac63bf711df43021436d24b542dee
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-20 07:39:21 -0800

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+/**
+ * main - tebahpla
+ * Return: always 0
+ */
+int main(void)
+{
+	char tebahpla;
+
+	for (tebahpla = 'z' ; tebahpla <= 'a' ; tebahpla++)
+	{
+		putchar(tebahpla);
+	}
+	putchar('\n');
+	return (0);
+}
