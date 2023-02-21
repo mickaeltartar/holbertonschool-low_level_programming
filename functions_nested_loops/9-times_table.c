9f2c54a9b7619560a7769bdbc624e8edfc7de468
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-21 13:12:30 -0800

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+void times_table(void)
+	{
+	int num, mult, calc;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			calc = num * mult;
+
+			if (calc <= 9)
+				_putchar(' ');
+			else
+				_putchar((calc / 10) + '0');
+
+			_putchar((calc % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
