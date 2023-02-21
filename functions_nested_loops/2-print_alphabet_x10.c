43a1ca34391772bda5ff6d8486d9be930d347dfc
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-21 05:57:40 -0800

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write alphabet X10
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char az;
+	int a = 0;
+
+	while (a < 10)
+	{
+
+	for (az = 'a' ; az <= 'z' ; az++)
+
+	_putchar(az);
+	a++;
+	_putchar('\n');
+	}
+}
