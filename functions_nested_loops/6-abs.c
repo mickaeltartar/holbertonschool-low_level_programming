e260fc941ce64a48c6768411d76a88ab70787c69
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-21 08:18:53 -0800

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Calcul la valeur absolu de int.
+ *
+ * @n: nombre.
+ *
+ * Return: la valeur absolue de int.
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
