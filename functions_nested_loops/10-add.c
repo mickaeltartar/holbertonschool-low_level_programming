334e47d0feb5992926e09bc3cfbfa895680f334b
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-21 14:00:13 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: 10-add.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * add - Ajoute deux int.
+ * @num1: Ajout du premier int.
+ * @num2: Ajour du second int.
+ *
+ * Return: Le résultat de la somme.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
