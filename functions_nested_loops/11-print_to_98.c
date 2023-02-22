747834a672264bbdacebd84ef890481702ab8bea
mickaeltartar <mickael.tartar@hotmail.com>
2023-02-22 04:32:58 -0800

    11-print_to_98.c function to return 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural number n to 98 .
+ *
+ * @n: number .
+ */
+
+void print_to_98(int n)
+	{
+		if (n <= 98)
+
+			for (n = n; n <= 97; n++)
+				printf("%d, ", n);
+		else
+			for (n = n; n >= 97; n--)
+				printf("%d, ", n);
+		printf("98\n");
+	}
