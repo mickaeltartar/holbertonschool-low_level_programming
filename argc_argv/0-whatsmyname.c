b90403986a2f7d259c81279f4cc428bc0b9a1f1e
mickaeltartar <mickael.tartar@hotmail.com>
2023-03-07 14:32:19 -0800

    0-whatsmyname.c0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - Print the program name.
+ * @argc: command unused.
+ * @argv: An array of pointer to the arguments.
  *
- *
+ * Return: Return (0).
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-    char *ptr = argv[0];
-    printf("%s\n", ptr);
-    return (0);
+	char *ptr = argv[0];
+
+	printf("%s\n", ptr);
+
+	return (0);
 }
