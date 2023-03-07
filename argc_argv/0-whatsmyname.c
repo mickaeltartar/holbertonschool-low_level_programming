3ee1ce32a9a7266e567c416efc0a08a235e5f1da
mickaeltartar <mickael.tartar@hotmail.com>
2023-03-07 14:25:45 -0800

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ *
+ *
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+    char *ptr = argv[0];
+    printf("%s\n", ptr);
+    return (0);
+}
