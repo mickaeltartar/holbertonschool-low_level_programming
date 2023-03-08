6175702408de6213e80a572008b35a286e6364d8
mickaeltartar <mickael.tartar@hotmail.com>
2023-03-08 05:48:51 -0800

    100-times_table.c0

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,3 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+
 void print_times_table(int n)
 {
 	int i, j, k;
