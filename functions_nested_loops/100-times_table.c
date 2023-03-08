51752c5d956077f1ca0f001e8c2758557a0a43f7
mickaeltartar <mickael.tartar@hotmail.com>
2023-03-08 05:46:22 -0800

    100-times_table.c

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (!(n >= 0 && n <= 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j > 0)
+				{
+					printf("%4d", k);
+				}
+				else
+				{
+					printf("%d", k);
+				}
+			}
+			printf("\n");
+		}
+	}
+}
