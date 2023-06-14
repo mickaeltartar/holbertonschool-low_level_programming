57094803333ed908b63a528fc7ee59b3a6e33f3c
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-14 07:19:40 -0700

    fix vagrant 32 & 64

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,12 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)", sizeof(char));
-	printf("Size of an int: %zu bytes(s)", sizeof(int));
-	printf("Size of a long int: %zu bytes(s)", sizeof(long int));
-	printf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
-	printf("size of a float: %zu bytes(s)", sizeof(float));
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %zu bytes(s)\n", sizeof(float));
+
 	return (0);
 }
