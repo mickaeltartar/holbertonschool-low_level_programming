b114f8f6e993d89c22cb113a161515257bd2f7b6
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-14 04:48:05 -0700

    write a C program using puts for print

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
