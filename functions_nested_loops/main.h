7df304b78ab0a28615ef0add5c80ee01a1b4ecdd
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-20 00:56:00 -0700

    add readme, main.h and print putchar

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*******Library*******/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*******Prototype*******/
+int _putchar(char c);
+
+#endif
