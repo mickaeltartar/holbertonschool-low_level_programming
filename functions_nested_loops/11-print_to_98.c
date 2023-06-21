996e1a3384c66581cc107de68dbca6fe1edd8169
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-21 05:57:56 -0700

    prints all natural numbers started by n

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers start by n
+ * @n: the number
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
+}
