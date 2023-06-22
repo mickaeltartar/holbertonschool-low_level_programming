3b8e6ab75089a46c8818dac26cc94f53d37d1714
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-22 05:00:27 -0700

    draw a square in the terminal

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - draw a square in the terminal
+ * @size: the size
+ */
+
+void print_square(int size)
+{
+	int row = 0;
+	int column;
+
+	if (size > 0)
+	{
+
+		while(row < size)
+		{
+			column = 0;
+			while(column < size)
+			{
+				_putchar('#');
+				column++;
+			}
+			_putchar('\n');
+			row++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+			
