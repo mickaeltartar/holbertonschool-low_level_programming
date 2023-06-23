392232113188adc820d1c0783c10634500ea9419
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-23 04:35:48 -0700

    print 9 times table starting with 0

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - print 9 table starting with 0
+ * @n: the number
+ */
+
+void print_times_table(int n)
+{
+	int row, column, multi;
+
+	if (n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				multi = (row * column);
+
+				if (column == 0)
+					_putchar('0' + multi);
+				else if (multi <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + multi);
+				}
+				else if (multi <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(multi / 10 + '0');
+					_putchar(multi % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(multi / 100 + '0');
+					_putchar(multi % 100 + '0');
+					_putchar(multi % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
