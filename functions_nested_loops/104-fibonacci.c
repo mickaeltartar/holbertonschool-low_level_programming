949da35a0f4bf93848b13e008830589247e780d4
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-23 07:21:06 -0700

    fix correct output

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,23 +8,49 @@
 
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 1;
-	unsigned long sum;
-	int count;
+	int count = 2;
+	unsigned long currentFibonacci = 1;
+	unsigned long previousFibonacci = 1;
+	unsigned long nextFibonacci = 0;
+	unsigned long currentHead;
+	unsigned long currentTail;
+	unsigned long previousHead;
+	unsigned long previousTail;
+	int overflow;
+	unsigned long nextTail;
+	unsigned long nextHead;
 
 	printf("1");
 
-	for (count = 2; count < 99; count++)
+	while (count < 93)
 	{
-		sum = a + b;
-		a = b;
-		b = sum;
-		printf(", %lu", sum);
+		nextFibonacci = currentFibonacci + previousFibonacci;
+		previousFibonacci = currentFibonacci;
+		currentFibonacci = nextFibonacci;
+		printf(", %lu", nextFibonacci);
+		count++;
 	}
+	currentHead = currentFibonacci / 1000000000;
+	currentTail = currentFibonacci % 1000000000;
+	previousHead = previousFibonacci / 1000000000;
+	previousTail = previousFibonacci % 1000000000;
 
+	while (count < 99)
+	{
+		overflow = (currentTail + previousTail) / 1000000000;
+		nextTail = (currentTail + previousTail) - (1000000000 * overflow);
+		nextHead = (currentHead + previousHead) + overflow;
+
+		printf(", %lu%lu", nextHead, nextTail);
+
+		currentHead = previousHead;
+		currentTail = previousTail;
+		previousHead = nextHead;
+		previousTail = nextTail;
+
+		count++;
+	}
 	printf("\n");
+
 	return (0);
 }
-
-
