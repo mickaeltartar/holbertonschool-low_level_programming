bc5fc0e1f17db606df5e5eb8dba02d74c81bc266
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-26 02:00:43 -0700

    Fix correct output and add betty documentation style

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _puts - print a string
+ * @str: the string
  */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		str++;
 		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
