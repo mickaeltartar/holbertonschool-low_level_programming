c4f96b5ae0ff17e4e6e2029723a11643aef41593
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-26 05:19:14 -0700

    copy the string from dest to src

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenght of the string
+ * @s: the string
+ * Return: the lenght
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		lenght++;
+	}
+	return (lenght);
+}
+
+/**
+ * _strcpy - copy string from src to dest
+ * @dest: the destination
+ * @src: the source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+	while(src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+
+	return(dest);
+}
