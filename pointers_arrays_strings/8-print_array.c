cb829a7087e570abc8f1317bc2cdd02b299dfe7b
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-26 04:37:52 -0700

    print element of an array of integers followed by a new line

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int index = 0;
+
+	while (index < n)
+	{
+		if (index != (n - 1))
+		{
+			printf("%d, ",a[index]);
+		}
+		else
+		{
+			printf("%d", a[index]);
+		}
+		index++;
+	}
+	printf("\n");
+}
