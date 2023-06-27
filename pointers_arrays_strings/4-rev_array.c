0cdd634029c221cdbdc559fbe90808b6b7f148dc
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-27 08:17:12 -0700

    reverse the of an array of integer

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse content of an array integer
+ * @a: an array of integer
+ * @n the number of element to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		temp = *(a + start);
+		*(a + start) = *(a + end);
+		*(a + end) = temp;
+		start++;
+		end--;
+	}
+}
