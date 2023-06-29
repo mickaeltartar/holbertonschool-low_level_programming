e6528324c9e7f7585467d219e6e359b87559fc6e
mickaeltartar <mickael.tartar@hotmail.com>
2023-06-29 03:16:58 -0700

    locate a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - char in a string
+ * @s: the string
+ * @c: the caracter
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
 /****** LIBRARIES ******/
 
 #include <stdlib.h>
@@ -32,5 +31,6 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
