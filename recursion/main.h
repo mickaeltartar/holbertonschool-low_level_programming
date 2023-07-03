dd409b6f3a301f502b88597a6da2cfe218295bb1
mickaeltartar <mickael.tartar@hotmail.com>
2023-07-03 01:27:43 -0700

    print a string followed by a new line

---
+++ recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/****** Libraries ******/
+#include <stdlib.h>
+#include <stdio.h>
+
+/****** Prototype ******/
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif
