e538856c50767a6da7253c5af23a4bb29a694f7a
mickaeltartar <mickael.tartar@hotmail.com>
2023-07-04 02:08:09 -0700

    add readme & main & write a fonction that print my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+
+		printf("%s\n", argv [0]);
+
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/****** Libraries ******/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/****** Prototypes ******/
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif
