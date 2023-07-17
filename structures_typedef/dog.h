0c08a4b12fc8d0ffef517610441aa3e4bcbbc9d3
mickaeltartar <mickael.tartar@hotmail.com>
2023-07-17 00:09:07 -0700

    betty documentation style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,24 +1,20 @@
-#ifndef DOG_H_
-#define DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * dog_t - typedeffor struct dog
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 /**
  * struct dog - structure for dogs
  * @name: first argument
  * @age: second argument
- * @owner: last argument
+ * @owner: third argument
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-#endif
