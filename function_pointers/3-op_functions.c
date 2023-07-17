e5ff539d4bb82595f183c8863544a1dfe26c66ce
mickaeltartar <mickael.tartar@hotmail.com>
2023-07-17 05:46:25 -0700

    program that performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+/**
+ * op_add - add
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *
+ * op_sub - substract
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: first nulmber
+ * @b: second number
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: first nulmber
+ * @b: second number
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus for remainder
+ * @a: first nulmber
+ * @b: second number
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
