3a4da078fb84f5088769aad5d005a8d2adeb16d9
mickaeltartar <mickael.tartar@hotmail.com>
2023-08-07 04:04:37 -0700

    add readme & main & function that reads a text file and prints it to the POSIX standard output

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print it in to the POSIX output
+ * @fileName: the given file
+ * @letters: is the number of letters should read
+ * Return: the actual number of letters to be read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileDescriptor;
+	ssize_t bytesRead, bytesWritten;
+	char *buffer;
+
+	buffer = malloc((letters) * sizeof(char));
+	if (!buffer)
+		return (0);
+	if (!filename)
+	{
+		free(buffer);
+		return (0);
+	}
+	fileDescriptor = open(filename, O_RDONLY);
+	if (fileDescriptor < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	bytesRead = read(fileDescriptor, buffer, letters);
+	if (bytesRead < 0)
+	{
+		free(buffer);
+		close(fileDescriptor);
+		return (0);
+	}
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten != bytesRead || bytesWritten < 0)
+	{
+		free(buffer);
+		close(fileDescriptor);
+		return (0);
+	}
+	close(fileDescriptor);
+	free(buffer);
+	return (bytesWritten);
+}
---
+++ file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/****** libraries ******/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/****** prototype ******/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
