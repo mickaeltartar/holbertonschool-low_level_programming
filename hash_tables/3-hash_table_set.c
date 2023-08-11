0ffc4e45b8e67e9a3b7c914c6fa75780c3f922ee
mickaeltartar <mickael.tartar@hotmail.com>
2023-08-11 11:04:06 +0200

    add file compiled and add betty documentation style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,13 +1,17 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
- * 
+ * hash_table_set - add an element to the hash table
+ * @ht: given hash table pointer
+ * @key: the given key
+ * @value: the value given by the key
+ * Return: 1 is success otherwise 0
 */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_table_t, *newNode, *tmp;
+	hash_node_t *newNode, *tmp;
 
 	if (!ht || !key || !*key || !value)
 	return (0);
@@ -15,15 +19,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *) key, ht->size);
 	tmp = ht->array[index];
 
-	while(tmp)
+	while (tmp)
 	{
-		if(!strcmp (key, tmp->key))
+		if (!strcmp(key, tmp->key))
 		{
 			free(tmp->value);
 			tmp->value = strdup(value);
 				if (!tmp->value)
 				return (0);
-			return (1)
+			return (1);
 		}
 		tmp = tmp->next;
 	}
@@ -35,9 +39,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(newNode);
 		free(newNode->key);
-		return(0);
+		return (0);
 	}
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
-	return(1)
+	return (1);
 }
