7d8e8387a9f84afd5c5ce2bb303a38240b307a59
mickaeltartar <mickael.tartar@hotmail.com>
2023-09-19 15:41:58 +0200

    add readme & file with function that searches for a value in an array of integers aith algo

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for value in an array
+ * using a linear search algorithm
+ * @array: pointer to first element of an array
+ * @size: size of the array
+ * @value: value to be found in array
+ * Return: first index if value is find in a array otherwise return -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	for (index = 0; index < size; index++)
+	{
+		printf("value checked array[%ld] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+	}
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/****** Libraries ******/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/****** Prototypes ******/
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
