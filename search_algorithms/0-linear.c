0f85c9a4fbec09fa1b7ec564e8b958d1492cd1ad
mickaeltartar <mickael.tartar@hotmail.com>
2023-09-19 15:44:47 +0200

    fix correct output

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,9 +13,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
+	if (array == NULL)
+		return (-1);
+
 	for (index = 0; index < size; index++)
 	{
-		printf("value checked array[%ld] = [%d]\n", index, array[index]);
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
 	}
